a3b2f96ac8241d5325ac5e6fdfe07f0f92c7fdc7
Griffin Annshual <griffintbr@gmail.com>
2024-07-24 00:54:56 +0530

    bug fixes

--- enrichiq_frontend/src/utils/endpoint.jsx
+++ enrichiq_frontend/src/utils/endpoint.jsx
@@ -1,12 +1,10 @@
-import process from "node:process"
-
 const server_end_point =
-	process.env.NODE_ENV !== "production"
+	import.meta.env.NODE_ENV !== "production"
 		? "http://localhost:4000"
 		: "https://enrich-iq-backend.vercel.app"
 
 const client_endpoint =
-	process.env.NODE_ENV !== "production"
+	import.meta.env.NODE_ENV !== "production"
 		? "http://localhost:5173"
 		: "https://dev-enrichiq.vercel.app"
 
