67b2d8657bfb8493183844cb2ce94799df9e4ebc
GriffinAnnshual <griffintbr@gmail.com>
2024-08-16 18:16:23 +0530

    update price configs for production

--- enrichiq_frontend/src/constant/constants.jsx
+++ enrichiq_frontend/src/constant/constants.jsx
@@ -1,54 +1,54 @@
 // // production config
 
-// export const planConfiguration = {
-// 	price_1PoOBDSIJMxI4tFFoCwU1vNp: "Basic",
-// 	price_1PoOBOSIJMxI4tFFDKbqiOHj: "Pro",
-// 	price_1PoOBXSIJMxI4tFFtF9otSis: "Premium",
-// 	price_1PoOEXSIJMxI4tFFOjaXb57n: "Basic",
-// 	price_1PoOEmSIJMxI4tFFTPCkrDKS: "Pro",
-// 	price_1PoOF0SIJMxI4tFFOp2IvdZG: "Premium",
-// }
-
-// export const priceConfiguration = {
-// 	price_1PoOBDSIJMxI4tFFoCwU1vNp: 100,
-// 	price_1PoOBOSIJMxI4tFFDKbqiOHj: 200,
-// 	price_1PoOBXSIJMxI4tFFtF9otSis: 300,
-// 	price_1PoOEXSIJMxI4tFFOjaXb57n: 100,
-// 	price_1PoOEmSIJMxI4tFFTPCkrDKS: 200,
-// 	price_1PoOF0SIJMxI4tFFOp2IvdZG: 300,
-// }
-
-// export const globalPriceList = [
-// 	"price_1PoOBDSIJMxI4tFFoCwU1vNp",
-// 	"price_1PoOBOSIJMxI4tFFDKbqiOHj",
-// 	"price_1PoOBXSIJMxI4tFFtF9otSis",
-// ]
-
-// testing config
-
 export const planConfiguration = {
-	price_1PT5TySIJMxI4tFFK6hXKXwb: "Basic",
-	price_1PT5UzSIJMxI4tFFgTC43kos: "Pro",
-	price_1PT5VsSIJMxI4tFFdnnarfss: "Premium",
-	price_1PT5UlSIJMxI4tFF701TNHj5: "Basic",
-	price_1PT5VZSIJMxI4tFFzOxDJkEr: "Pro",
-	price_1PT5VHSIJMxI4tFFubCUjlB1: "Premium",
+	price_1PoOBDSIJMxI4tFFoCwU1vNp: "Basic",
+	price_1PoOBOSIJMxI4tFFDKbqiOHj: "Pro",
+	price_1PoOBXSIJMxI4tFFtF9otSis: "Premium",
+	price_1PoOEXSIJMxI4tFFOjaXb57n: "Basic",
+	price_1PoOEmSIJMxI4tFFTPCkrDKS: "Pro",
+	price_1PoOF0SIJMxI4tFFOp2IvdZG: "Premium",
 }
 
 export const priceConfiguration = {
-	price_1PT5TySIJMxI4tFFK6hXKXwb: 100,
-	price_1PT5UzSIJMxI4tFFgTC43kos: 200,
-	price_1PT5VsSIJMxI4tFFdnnarfss: 300,
-	price_1PT5UlSIJMxI4tFF701TNHj5: 100,
-	price_1PT5VZSIJMxI4tFFzOxDJkEr: 200,
-	price_1PT5VHSIJMxI4tFFubCUjlB1: 300,
+	price_1PoOBDSIJMxI4tFFoCwU1vNp: 100,
+	price_1PoOBOSIJMxI4tFFDKbqiOHj: 200,
+	price_1PoOBXSIJMxI4tFFtF9otSis: 300,
+	price_1PoOEXSIJMxI4tFFOjaXb57n: 100,
+	price_1PoOEmSIJMxI4tFFTPCkrDKS: 200,
+	price_1PoOF0SIJMxI4tFFOp2IvdZG: 300,
 }
 
 export const globalPriceList = [
-	"price_1PT5TySIJMxI4tFFK6hXKXwb",
-	"price_1PT5UzSIJMxI4tFFgTC43kos",
-	"price_1PT5VsSIJMxI4tFFdnnarfss",
+	"price_1PoOBDSIJMxI4tFFoCwU1vNp",
+	"price_1PoOBOSIJMxI4tFFDKbqiOHj",
+	"price_1PoOBXSIJMxI4tFFtF9otSis",
 ]
+
+// testing config
+
+// export const planConfiguration = {
+// 	price_1PT5TySIJMxI4tFFK6hXKXwb: "Basic",
+// 	price_1PT5UzSIJMxI4tFFgTC43kos: "Pro",
+// 	price_1PT5VsSIJMxI4tFFdnnarfss: "Premium",
+// 	price_1PT5UlSIJMxI4tFF701TNHj5: "Basic",
+// 	price_1PT5VZSIJMxI4tFFzOxDJkEr: "Pro",
+// 	price_1PT5VHSIJMxI4tFFubCUjlB1: "Premium",
+// }
+
+// export const priceConfiguration = {
+// 	price_1PT5TySIJMxI4tFFK6hXKXwb: 100,
+// 	price_1PT5UzSIJMxI4tFFgTC43kos: 200,
+// 	price_1PT5VsSIJMxI4tFFdnnarfss: 300,
+// 	price_1PT5UlSIJMxI4tFF701TNHj5: 100,
+// 	price_1PT5VZSIJMxI4tFFzOxDJkEr: 200,
+// 	price_1PT5VHSIJMxI4tFFubCUjlB1: 300,
+// }
+
+// export const globalPriceList = [
+// 	"price_1PT5TySIJMxI4tFFK6hXKXwb",
+// 	"price_1PT5UzSIJMxI4tFFgTC43kos",
+// 	"price_1PT5VsSIJMxI4tFFdnnarfss",
+// ]
 
 export const accordionData = [
 	{
